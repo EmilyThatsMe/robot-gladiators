88693d3bf74dbdcafb021d2e8eb5b331b57b4e6c
EmilyT <emilython@hotmail.com>
2021-08-23 13:44:09 -0500

    storing data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+//this creates a function named "fight"
+var playername = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playername);
+
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Out robot's name is " + playername);
+
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
+
