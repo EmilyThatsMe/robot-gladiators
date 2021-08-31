5a01e4e117a3972eb766b70bccb5045f659af744
EmilyT <emilython@hotmail.com>
2021-08-30 20:40:15 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,13 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 //fight function
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -36,7 +43,11 @@ var fight = function(enemyName) {
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
+
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -46,7 +57,7 @@ var fight = function(enemyName) {
         window.alert(enemyName + ' has died!');
   
         // award player money for winning
-        playerMoney = playerMoney + 20;
+        playerMoney = Math.max(0, playerMoney - 10);
   
         // leave while() loop since enemy is dead
         break;
@@ -55,7 +66,10 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damage);
+      
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -89,7 +103,7 @@ for (var i = 0; i < enemyNames.length; i++) {
       var pickedEnemyName = enemyNames[i];
   
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
   
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
